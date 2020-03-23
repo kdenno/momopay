b97660ee0c7fb1f8a084517bfd78d9120bbb6660
kdenno <kabuyedennis3@gmail.com>
2020-03-23 11:09:00 +0300

    updated gateway plugin code

--- arnold-gateway.php
+++ arnold-gateway.php
@@ -8,67 +8,98 @@
  * Version: 1.0.2
  *
  */
- 
- /*
+
+/*
  * This action hook registers our PHP class as a WooCommerce payment gateway
  */
+defined('ABSPATH') or exit;
 
-
-add_filter( 'woocommerce_payment_gateways', 'arnold_add_gateway_class' );
-function arnold_add_gateway_class( $gateways ) {
-	$gateways[] = 'WC_Arnold_Gateway'; // your class name is here
-	return $gateways;
+// Make sure WooCommerce is active
+if (!in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
+    return;
 }
- 
+
+// define plugin url
+if (!defined('ARNOLDGATEWAY_URL')) {
+    define('ARNOLDGATEWAY_URL', plugins_url('', __FILE__) . '/');
+}
+
+function wc_arnold_add_to_gateways($gateways)
+{
+    $gateways[] = 'WC_Arnold_Gateway';
+    return $gateways;
+}
+add_filter('woocommerce_payment_gateways', 'wc_arnold_add_to_gateways');
+
+// add plugin page links
+
+function wc_arnold_gateway_plugin_links($links)
+{
+
+    $plugin_links = array(
+        '<a href="' . admin_url('admin.php?page=wc-settings&tab=checkout&section=arnold_gateway') . '">' . __('Configure', 'wc-arnold-gateway') . '</a>',
+    );
+
+    return array_merge($plugin_links, $links);
+}
+add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'wc_arnold_gateway_plugin_links');
+
 /*
  * The class itself, please note that it is inside plugins_loaded action hook
  */
-add_action( 'plugins_loaded', 'arnold_init_gateway_class' );
-function arnold_init_gateway_class() {
- 
-	class WC_Arnold_Gateway extends WC_Payment_Gateway {
- 
- 		/**
- 		 * Class constructor, more about it in Step 3
- 		 */
-          public function __construct() {
- 
-            $this->id = 'arnold'; // payment gateway plugin ID
+add_action('plugins_loaded', 'arnold_init_gateway_class', 11);
+function arnold_init_gateway_class()
+{
+
+    class WC_Arnold_Gateway extends WC_Payment_Gateway
+    {
+
+        /**
+         * Class constructor, more about it in Step 3
+         */
+        private $publishable_key, $private_key, $encodedstr;
+
+        public function __construct()
+        {
+
+            $this->id = 'arnold_gateway'; // payment gateway plugin ID
             $this->icon = ''; // URL of the icon that will be displayed on checkout page near your gateway name
-            $this->has_fields = true; // in case you need a custom credit card form
-            $this->method_title = 'Namisi Arnold Gateway';
-            $this->method_description = 'Powered By Namisi Arnold Paul, Check out using MTN MOMO PAY, supports MTN MObile Money'; // will be displayed on the options page
-         
+            $this->has_fields = false; // in case you need a custom credit card form
+            $this->method_title = __('Namisi Arnold Gateway', 'wc-arnold-gateway');
+            $this->method_description = __('Powered By Namisi Arnold Paul, Check out using MTN MOMO PAY, supports MTN MObile Money', 'wc-arnold-gateway'); // will be displayed on the options page
+
             // gateways can support subscriptions, refunds, saved payment methods,
             // but in this tutorial we begin with simple payments
             $this->supports = array(
                 'products'
             );
-         
+
             // Method with all the options fields
             $this->init_form_fields();
-         
+
             // Load the settings.
             $this->init_settings();
-            $this->title = $this->get_option( 'title' );
-            $this->description = $this->get_option( 'description' );
-            $this->enabled = $this->get_option( 'enabled' );
-            $this->testmode = 'yes' === $this->get_option( 'testmode' );
-            $this->private_key = $this->testmode ? $this->get_option( 'test_private_key' ) : $this->get_option( 'private_key' );
-            $this->publishable_key = $this->testmode ? $this->get_option( 'test_publishable_key' ) : $this->get_option( 'publishable_key' );
-         
+            $this->title = $this->get_option('title');
+            $this->description = $this->get_option('description');
+            $this->enabled = $this->get_option('enabled');
+            $this->testmode = 'yes' === $this->get_option('testmode');
+            $this->private_key = $this->testmode ? $this->get_option('test_private_key') : $this->get_option('private_key');
+            $this->publishable_key = $this->testmode ? $this->get_option('test_publishable_key') : $this->get_option('publishable_key');
+            $this->encodedstr = $this->get_option('encodedstr')
+
             // This action hook saves the settings
-            add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
-         
+            add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
+
             // You can also register a webhook here
             // add_action( 'woocommerce_api_{webhook name}', array( $this, 'webhook' ) );
-         }
- 
-		/**
- 		 * Plugin options, we deal with it in Step 3 too
- 		 */
-          public function init_form_fields(){
- 
+        }
+
+        /**
+         * Plugin options, we deal with it in Step 3 too
+         */
+        public function init_form_fields()
+        {
+
             $this->form_fields = array(
                 'enabled' => array(
                     'title'       => 'Enable/Disable',
@@ -113,16 +144,22 @@ function arnold_init_gateway_class() {
                 'private_key' => array(
                     'title'       => 'Live Private Key',
                     'type'        => 'password'
+                ),
+                'encodedstr' => array(
+                    'title'       => 'Your Base 64 Encoded String',
+                    'type'        => 'password'
                 )
             );
         }
- 
-		/**
-		 * You will need it if you want your custom credit card form, Step 4 is about it
-		 */
-		public function payment_fields() {
- 
-            // ok, let's display some description before the payment form
+
+        /**
+         * You will need it if you want your custom credit card form, Step 4 is about it
+         */
+        public function payment_fields()
+        {
+
+            // commenting out default credit card form
+            /*
             if ( $this->description ) {
                 // you can instructions for test mode, I mean test card numbers etc.
                 if ( $this->testmode ) {
@@ -150,133 +187,189 @@ function arnold_init_gateway_class() {
             do_action( 'woocommerce_credit_card_form_end', $this->id );
          
             echo '<div class="clear"></div></fieldset>';
-         
+            */
         }
- 
-		/*
+
+        /*
 		 * Custom CSS and JS, in most cases required only when you decided to go with a custom credit card form
 		 */
- 
-		/*
+
+        /*
  		 * Fields validation, more in Step 5
 		 */
- 
-		/*
+
+        /*
 		 * We're processing the payments here, everything about it is in Step 5
 		 */
 
-        public function process_payment( $order_id ) {
- 
-            global $woocommerce;
-         
-            // we need it to get any order detailes
-            $order = wc_get_order( $order_id );
-         
+        public function gen_uuid()
+        {
+            return sprintf(
+                '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+                // 32 bits for "time_low"
+                mt_rand(0, 0xffff),
+                mt_rand(0, 0xffff),
 
-            // This sample uses the Apache HTTP client from HTTP Components (http://hc.apache.org/httpcomponents-client-ga/)
-           // require_once 'HTTP/Request2.php';
+                // 16 bits for "time_mid"
+                mt_rand(0, 0xffff),
 
-            $request = new Http_Request2('https://sandbox.momodeveloper.mtn.com/collection/v1_0/requesttopay');
-            $url = $request->getUrl();
+                // 16 bits for "time_hi_and_version",
+                // four most significant bits holds version number 4
+                mt_rand(0, 0x0fff) | 0x4000,
 
-            function gen_uuid() {
-                return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-                    // 32 bits for "time_low"
-                    mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
-            
-                    // 16 bits for "time_mid"
-                    mt_rand( 0, 0xffff ),
-            
-                    // 16 bits for "time_hi_and_version",
-                    // four most significant bits holds version number 4
-                    mt_rand( 0, 0x0fff ) | 0x4000,
-            
-                    // 16 bits, 8 bits for "clk_seq_hi_res",
-                    // 8 bits for "clk_seq_low",
-                    // two most significant bits holds zero and one for variant DCE1.1
-                    mt_rand( 0, 0x3fff ) | 0x8000,
-            
-                    // 48 bits for "node"
-                    mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
-                );
-            }
+                // 16 bits, 8 bits for "clk_seq_hi_res",
+                // 8 bits for "clk_seq_low",
+                // two most significant bits holds zero and one for variant DCE1.1
+                mt_rand(0, 0x3fff) | 0x8000,
 
-            $headers = array(
-                // Request headers
-                'Authorization' => '',
-                'X-Callback-Url' => 'https://subulaug.com',
-                'X-Reference-Id' => gen_uuid(),
-                'X-Target-Environment' => '',
-                'Content-Type' => 'application/json',
-                'Ocp-Apim-Subscription-Key' => 'b4d4c22a538147068153346505f66ada',
-                
+                // 48 bits for "node"
+                mt_rand(0, 0xffff),
+                mt_rand(0, 0xffff),
+                mt_rand(0, 0xffff)
             );
-
-            $request->setHeader($headers);
-
-            $parameters = array(
-                // Request parameters
-            );
-
-            $url->setQueryVariables($parameters);
-
-            $request->setMethod(HTTP_Request2::METHOD_POST);
-
-            // Request body
-            $request->setBody('
-            {
-                "amount": "string",
-                "currency": "string",
-                "externalId": "string",
-                "payer": {
-                "partyIdType": "MSISDN",
-                "partyId": "0775307641"
-                },
-                "payerMessage": "Blob",
-                "payeeNote": "string"
-                
-            }');
-
-            try
-            {
-                $response = $request->send();
-                echo $response->getBody();
-                
-                $body = json_decode( $response->getBody(), true );
-                    
-                // it could be different depending on your payment processor
-                if ( $body['response']['status'] == 'SUCCESSFUL' ) {
-
-                // we received the payment
-                $order->payment_complete();
-                $order->reduce_order_stock();
-
-                // some notes to customer (replace true with false to make it private)
-                $order->add_order_note( 'Hey, your order is paid! Thank you!', true );
-
-                // Empty cart
-                $woocommerce->cart->empty_cart();
-
-                // Redirect to the thank you page
-                return array(
-                    'result' => 'success',
-                    'redirect' => $this->get_return_url( $order )
-                );
-
-                }
-                else {
-                    wc_add_notice(  'Please try again.', 'error' );
-                    
-                    
-                    return;
-                }
-            }
-            catch (HttpException $ex)
-            {
-                echo $ex;
-            }
-         
         }
-		
+
+        public function process_payment($order_id)
+        {
+
+            global $woocommerce;
+
+            // we need it to get any order detailes
+            $order = wc_get_order($order_id);
+
+            // commenting out Http_Request2, gonna harness wp_get/remote_post
+            // $request = new Http_Request2('https://sandbox.momodeveloper.mtn.com/collection/v1_0/requesttopay');
+            // $url = $request->getUrl();
+
+            $randomID = $this->gen_uuid();
+
+            $url = 'https://sandbox.momodeveloper.mtn.com/collection/v1_0/token';
+            $url2 = 'https://sandbox.momodeveloper.mtn.com/collection/v1_0/requesttopay';
+            $encodedstr = $this->encodedstr;// enter b64 string;
+            $subkey = $this->publishable_key;
+            //$order = wc_get_order( $order_id );
+            $order = new WC_Order($order_id);
+            $order_data = $order->get_data();
+            $total_amount = $order_data['total'];
+            $items = $order->get_items();
+            $itemName;
+            $phoneno = $order_data['billing']['phone'];
+            $phone_number;
+            $firstdig = substr($phoneno, 0, 1);
+            $firstdigits = substr($phoneno, 0, 3);
+
+            if ($firstdig === '0') {
+                $phone_number = preg_replace('/^0/', '256', $phoneno);
+            } elseif ($firstdigits === '256') {
+                $phone_number = $phoneno;
+            } else {
+                $error_message = 'Please provide a valid MTN number (should start with 07 or 256)';
+                throw new Exception(__('Number invalid. ' . $error_message, 'wc-arnold-gateway'));
+            }
+            // get order name
+            foreach ($items as $item) {
+                $itemName = $item['name'];
+            };
+
+            // get tokens
+            $response = wp_remote_post(
+                $url,
+                array(
+
+                    "method" => "POST",
+                    "timeout" => 45,
+                    "redirection" => 5,
+                    "httpversion" => '1.0',
+                    "blocking" => true,
+                    "headers" => array(
+                        "Ocp-Apim-Subscription-Key" => $subkey,
+                        "Authorization" => "Basic " . $encodedstr,
+                        "Content-Type" => "application/json"
+                    )
+
+                )
+            );
+
+            if (is_wp_error($response)) {
+                $error_message = $response->get_error_message();
+                throw new Exception(__('There is a problem creating token. ' . $error_message, 'wc-arnold-gateway'));
+            }
+
+            $tknobj = json_decode($response['body']);
+            $thetoken = $tknobj->{'access_token'};
+            $auth = "Bearer " . $thetoken;
+            // process payment
+            $payresponse = wp_remote_post($url2, array(
+                "method" => "POST",
+                "timeout" => 45,
+                "redirection" => 5,
+                "httpversion" => '1.0',
+                "blocking" => true,
+                "headers" => array(
+                    "Ocp-Apim-Subscription-Key" => $subkey,
+                    "Authorization" => $auth,
+                    "X-Reference-Id" => $randomID,
+                    "X-Target-Environment" => "sandbox",
+                    "X-Callback-Url" => "call back url for your website",
+                    "cache-control" => "no-cache",
+                    "Content-Type" => "application/json"
+                ),
+                "body" => json_encode(array(
+                    "amount" => $total_amount,
+                    "currency" => "EUR",
+                    "externalId" => 'XXX',
+                    "payer" => array(
+                        "partyIdType" => "MSISDN",
+                        "partyId" => $phone_number
+                    ),
+                    "payerMessage" => "message",
+                    "payeeNote" => "string"
+                ), JSON_FORCE_OBJECT)
+            ));
+            if (is_wp_error($payresponse)) {
+                $error_message = $payresponse->get_error_message();
+                throw new Exception(__('There is a problem processing payment. ' . $error_message, 'wc-arnold-gateway'));
+            }
+
+            // check on payment
+            $getresponse = wp_remote_get("https://ericssonbasicapi1.azure-api.net/collection/v1_0/requesttopay/" . $randomID, array(
+                "headers" => array(
+                    "Ocp-Apim-Subscription-Key" => $subkey,
+                    "Authorization" => $auth,
+                    "X-Reference-Id" => $randomID,
+                    "X-Target-Environment" => "sandbox",
+                    "Content-Type" => "application/json"
+                )
+            ));
+            if (is_wp_error($payresponse)) {
+                $error_message = $payresponse->get_error_message();
+                throw new Exception(__('There is a problem processing payment. ' . $error_message, 'wc-arnold-gateway'));
+            }
+
+            $getrespobj = json_decode($getresponse['body']);
+            $getrespstat = $getrespobj->{'status'};
+            if ($getrespstat === 'FAILED') {
+                // Get status message
+                $error_message = $getrespobj->{'reason'};;
+                throw new Exception(__($error_message . ' Make sure phone number is registered with MTN Mobile Money and you have enough credit ', 'wc-arnold-gateway'));
+            }
+
+            // Mark as on-hold (we're awaiting the payment)
+            $order->update_status('on-hold', __('Awaits payment, refID: ' . $randomID, 'wc-arnold-gateway'));
+
+            //$order->payment_complete();
+
+            // Reduce stock levels
+            // $order->reduce_order_stock();
+
+            // Remove cart
+            WC()->cart->empty_cart();
+
+            // Return thankyou redirect
+            return array(
+                'result' => 'success',
+                'redirect' => $this->get_return_url($order),
+            );
+        }
     }
 }
